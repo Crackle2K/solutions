2d6cbd21a8ea243e5634dc0ea97a344d291e8c72
Dinesh Sinnathamby <103861462+Crackle2K@users.noreply.github.com>
2026-05-27 20:05:04 -0400

    Add Python script for translating text messages

---
+++ DMOJ/CCC/Python/CCC '07 J2 - I Speak TXTMSG.py
@@ -0,0 +1,25 @@
+
+b = {'CU': 'see you',
+     ':-)': "I'm happy",
+     ':-(': "I'm unhappy",
+     ';-)': 'wink',
+     ':-P': 'stick out my tongue',
+     '(~.~)': 'sleepy',
+     'TA': 'totally awesome',
+     'CCC': 'Canadian Computing Competition',
+     'CUZ': 'because',
+     'TY': 'thank-you',
+     "YW": "you're welcome",
+     'TTYL': 'talk to you later'}
+
+running = True
+
+while running:
+    message = input().strip()
+
+    print(b.get(message, message))
+
+    if message == "TTYL":
+        running = False
+
+    
